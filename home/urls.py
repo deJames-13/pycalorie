4afecbe0f2c6810fb3b48573ebd5ecf28e9b7b56
deJames-13 <de.james013@gmail.com>
2025-10-17 09:05:43 +0800

    Initial UI

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('theme/', views.theme_docs, name='theme'),
+    
 ]
--- home/views.py
+++ home/views.py
@@ -7,3 +7,7 @@ def index(request):
 
     # Page from the theme 
     return render(request, 'pages/index.html')
+
+def theme_docs(request):
+    """Render the Django Pixel theme documentation page."""
+    return render(request, 'docs/theme.html')
